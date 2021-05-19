832ddccb83d9762ba59cacd1b0850597eeb3e3f2
vlad <vladhstudios@gmail.com>
2021-05-19 14:21:47 +0200

    Revert "debug"
    This reverts commit e2c100614ad752895ff2ffe5d9f4dad8f58c5542.

--- entrypoint.sh
+++ entrypoint.sh
@@ -27,27 +27,27 @@ _git_changed() {
 
 # PROGRAM
 echo "Installing prettier..."
-# case $INPUT_PRETTIER_VERSION in
-#     false)
-#         npm install --silent --global prettier
-#         ;;
-#     *)
-#         npm install --silent --global prettier@$INPUT_PRETTIER_VERSION
-#         ;;
-# esac
+case $INPUT_PRETTIER_VERSION in
+    false)
+        npm install --silent --global prettier
+        ;;
+    *)
+        npm install --silent --global prettier@$INPUT_PRETTIER_VERSION
+        ;;
+esac
 
 # Install plugins
-# if [ -n "$INPUT_PRETTIER_PLUGINS" ]; then
-#     for plugin in $INPUT_PRETTIER_PLUGINS; do
-#         echo "Checking plugin: $plugin"
-#         # check regex against @prettier/xyz
-#         if ! echo "$plugin" | grep -Eq '(@prettier\/)+(plugin-[a-z\-]+)'; then
-#             echo "$plugin does not seem to be a valid @prettier/plugin-x plugin. Exiting."
-#             exit 1
-#         fi
-#     done
-#     npm install --silent --global $INPUT_PRETTIER_PLUGINS
-# fi
+if [ -n "$INPUT_PRETTIER_PLUGINS" ]; then
+    for plugin in $INPUT_PRETTIER_PLUGINS; do
+        echo "Checking plugin: $plugin"
+        # check regex against @prettier/xyz
+        if ! echo "$plugin" | grep -Eq '(@prettier\/)+(plugin-[a-z\-]+)'; then
+            echo "$plugin does not seem to be a valid @prettier/plugin-x plugin. Exiting."
+            exit 1
+        fi
+    done
+    npm install --silent --global $INPUT_PRETTIER_PLUGINS
+fi
 
 echo "Prettifing files..."
 echo "Files:"
